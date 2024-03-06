579d17be24cfbdfa41640e274f98388945092b56
davydovMikhail <davydov27977@gmail.com>
2024-03-06 15:58:30 +0300

    edits

--- src/components/tokenomics.tsx
+++ src/components/tokenomics.tsx
@@ -4,41 +4,41 @@ const Tokenomics = () => {
 
     return (
         <>
-           <div className="tokenomics">
-          <div className="tokenomics__title">
-            Tokenomics
-          </div>
-          <div className="tokenomics__blocks">
-            <div className="tokenomics__block1"></div>
-            <div className="tokenomics__block2"></div>
-            <div className="tokenomics__block3"></div>
-            <div className="tokenomics__percents">
-              <div className="tokenomics__percent">
-                <div className="tokenomics__percent_value">
-                  84%
-                </div>
-                <div className="tokenomics__percent_name">
-                Liquidity
-                </div>
-              </div>
-              <div className="tokenomics__percent">
-                <div className="tokenomics__percent_value">
-                  10%
-                </div>
-                <div className="tokenomics__percent_name">
-                Bank
-                </div>
-              </div>
-              <div className="tokenomics__percent">
-                <div className="tokenomics__percent_value">
-                  6%
-                </div>
-                <div className="tokenomics__percent_name">
-                  Team
-                </div>
-              </div>
+        <div className="tokenomics">
+            <div className="tokenomics__title">
+                Tokenomics
             </div>
-          </div>
+            <div className="tokenomics__blocks">
+                <div className="tokenomics__block_liq"></div>
+                <div className="tokenomics__block_bank"></div>
+                <div className="tokenomics__block_team"></div>
+                <div className="tokenomics__percents">
+                    <div className="tokenomics__percent">
+                        <div className="tokenomics__percent_value">
+                        84%
+                        </div>
+                        <div className="tokenomics__percent_name">
+                        Liquidity
+                        </div>
+                    </div>
+                    <div className="tokenomics__percent">
+                        <div className="tokenomics__percent_value">
+                        10%
+                        </div>
+                        <div className="tokenomics__percent_name">
+                        Bank
+                        </div>
+                    </div>
+                    <div className="tokenomics__percent">
+                        <div className="tokenomics__percent_value">
+                        6%
+                        </div>
+                        <div className="tokenomics__percent_name">
+                        Team
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div> 
         </>
